404bac72c1ee6b4572249d2503edd1bf69b962fe
pseudo-ankit <ankit.singh119219@gmail.com>
2025-01-31 22:09:10 +0530

    inital setup

---
+++ src/test/java/dev/aks/spring_ai/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aks.spring_ai.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
